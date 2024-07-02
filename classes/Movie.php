d63ce36ebb8f783de54733afe15976d2d6446da4
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-02 16:12:36 +0200

    listed information , and calculate general profit with a method

--- classes/Movie.php
+++ classes/Movie.php
@@ -2,17 +2,27 @@
 
 class Movie
 {
+    // ? Dichiarazione di visibilità . Variabili di istanza
     public $title;
     public $airedFirstTime;
     public $budget;
-    public $profit;
+    public $moneyGained;
     public $genre;
-
-    public function __construct($title, $airedFirstTime, $budget, $profit, $genre)
+    public $duration;
+    public $profit = 0;
+    // ! funzione di costruzione , inserisci i valori delle VarIst quando istanzio la variabile nel mio index.php
+    public function __construct($title, $airedFirstTime, $budget, $moneyGained, $genre, $duration)
     {
         $this->title = $title;
         $this->airedFirstTime = $airedFirstTime;
         $this->budget = $budget;
-        $this->profit = $profit;
+        $this->moneyGained = $moneyGained;
+        $this->genre = $genre;
+        $this->duration = $duration;
+    }
+
+    public function totalProfit($moneyGained, $budget)
+    {
+        return $this->profit = $moneyGained - $budget;
     }
 }
--- index.php
+++ index.php
@@ -1,7 +1,68 @@
 <?php
 include_once __DIR__ . '/classes/Movie.php';
-$matrix = new Movie("matrix", "18-4-1999", "200kk", "1kkk", "action");
 
-$lotrl = new Movie("The lord of the rings", "18-4-2322", "400kk", "2kkk", "fantasy");
+$matrixFirst = new Movie("The Matrix", "March 24, 1999", 63000000, 467600000, "action", 136,);
 
-var_dump($matrix, $lotrl);
+$lotrFirst = new Movie("Fellowship of the ring", "10 December 2001", 93000000, 898200000, "fantasy", 178,);
+
+$matrixFirstProfit = $matrixFirst->totalProfit($matrixFirst->moneyGained, $matrixFirst->budget);
+$lotrFirstProfit =  $lotrFirst->totalProfit($lotrFirst->moneyGained, $lotrFirst->budget);
+// var_dump($matrixFirst, $lotrFirst);
+?>
+
+<header>
+    <h1>Films infos</h1>
+</header>
+
+<section>
+    <ul>
+        <li>
+            <h1> <?php echo $matrixFirst->title ?></h1>
+        </li>
+        <li>
+            Aired in <?php echo $matrixFirst->airedFirstTime ?>
+        </li>
+        <li>
+            Initial budget <?php echo $matrixFirst->budget ?>
+        </li>
+        <li>
+            Money gained from box office <?php echo $matrixFirst->moneyGained ?>
+        </li>
+        <li>
+            Genre : <?php echo $matrixFirst->genre ?>
+        </li>
+        <li>
+            Film duration <?php echo $matrixFirst->duration ?> minutes
+        </li>
+    </ul>
+</section>
+
+<section>
+    <ul>
+        <li>
+            <h1> <?php echo $lotrFirst->title ?></h1>
+        </li>
+        <li>
+            Aired in <?php echo $lotrFirst->airedFirstTime ?>
+        </li>
+        <li>
+            Initial budget <?php echo $lotrFirst->budget ?>
+        </li>
+        <li>
+            Money gained from box office <?php echo $lotrFirst->moneyGained ?>
+        </li>
+        <li>
+            Genre : <?php echo $lotrFirst->genre ?>
+        </li>
+        <li>
+            Film duration <?php echo $lotrFirst->duration ?> minutes
+        </li>
+    </ul>
+</section>
+
+
+
+<div>
+    <p>Profit Matrix considering only budget and box office receipts : <?php echo $matrixFirstProfit; ?> $</p>
+    <p>Profit The Lord of The Rings considering only budget and box office receipts : <?php echo $lotrFirstProfit; ?> $</p>
+</div>
